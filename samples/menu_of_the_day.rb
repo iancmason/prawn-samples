68ba3b5f7f82c0a28a616898c9a8a64c9a1aa755
Eric Chiang <eric.ti.yu.chiang@gmail.com>
2014-12-14 18:34:17 -0500

    add title, date, and course headings

--- samples/menu_of_the_day.rb
+++ samples/menu_of_the_day.rb
@@ -8,4 +8,21 @@ pdf.font 'Helvetica'
 pdf.define_grid(columns: 5, rows: 8, gutter: 5)
 pdf.grid.show_all
 
+pdf.grid([0,1],[0,3]).bounding_box do
+  pdf.text 'Hidden Gem', size: 36, align: :center
+  pdf.text Date.today.to_s, size: 24, align: :center
+end
+
+pdf.grid([1,1],[2,3]).bounding_box do
+  pdf.text 'Appetizers', size: 24, align: :center
+end
+
+pdf.grid([3,1],[5,3]).bounding_box do
+  pdf.text 'Entrees', size: 24, align: :center
+end
+
+pdf.grid([6,1],[7,3]).bounding_box do
+  pdf.text 'Desserts', size: 24, align: :center
+end
+
 pdf.render_file File.expand_path('../../output/menu_of_the_day.pdf', __FILE__)
